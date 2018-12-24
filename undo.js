9b6b78142b61d0fc886382852988955a3190b584
Unknown <fredhgy2010@gmail.com>
2018-12-24 14:39:59 +0100

    fix undo bug, adjust possibility

--- undo.js
+++ undo.js
@@ -11,60 +11,66 @@ function undo() {
         counter = counter + mathcost[4] - 1;
         setCookie("counter", counter, "30");
         document.getElementById("counter").innerHTML = counter;
-        if (cal2 == cs1) {
-            document.getElementById("cbutton1").style.display = "block";
-        } else if (cal2 == cs2) {
-            document.getElementById("cbutton2").style.display = "block";
-        } else if (cal2 == cs3) {
-            document.getElementById("cbutton3").style.display = "block";
-        } else if (cal2 == "") {
-            return;
-        } else {
-            numberleft[cal2]++;
-            numberleftdisplay();
+        if (cal2 != 0) {
+            if (cal2 == cs1) {
+                document.getElementById("cbutton1").style.display = "block";
+            } else if (cal2 == cs2) {
+                document.getElementById("cbutton2").style.display = "block";
+            } else if (cal2 == cs3) {
+                document.getElementById("cbutton3").style.display = "block";
+            } else if (cal2 == "") {
+                //return;
+            } else {
+                numberleft[cal2]++;
+                numberleftdisplay();
+            }
         }
     } else if (cal1 == "joint()") {
         numberleft[11]++;
         numberleftdisplay();
-        if (cal2 == cs1) {
-            document.getElementById("cbutton1").style.display = "block";
-        } else if (cal2 == cs2) {
-            document.getElementById("cbutton2").style.display = "block";
-        } else if (cal2 == cs3) {
-            document.getElementById("cbutton3").style.display = "block";
-        } else if (cal2 == "") {
-            return;
-        } else {
-            numberleft[cal2]++;
-            numberleftdisplay();
+        if (cal2 != 0) {
+            if (cal2 == cs1) {
+                document.getElementById("cbutton1").style.display = "block";
+            } else if (cal2 == cs2) {
+                document.getElementById("cbutton2").style.display = "block";
+            } else if (cal2 == cs3) {
+                document.getElementById("cbutton3").style.display = "block";
+            } else if (cal2 == "") {
+                //return;
+            } else {
+                numberleft[cal2]++;
+                numberleftdisplay();
+            }
         }
-
-        if (cal3 == cs1) {
-            document.getElementById("cbutton1").style.display = "block";
-        } else if (cal3 == cs2) {
-            document.getElementById("cbutton2").style.display = "block";
-        } else if (cal3 == cs3) {
-            document.getElementById("cbutton3").style.display = "block";
-        } else if (cal3 == "") {
-            return;
-        } else {
-            numberleft[cal3]++;
-            numberleftdisplay();
+        if (cal3 != 0) {
+            if (cal3 == cs1) {
+                document.getElementById("cbutton1").style.display = "block";
+            } else if (cal3 == cs2) {
+                document.getElementById("cbutton2").style.display = "block";
+            } else if (cal3 == cs3) {
+                document.getElementById("cbutton3").style.display = "block";
+            } else if (cal3 == "") {
+                return;
+            } else {
+                numberleft[cal3]++;
+                numberleftdisplay();
+            }
         }
     } else {
-        if (cal1 == cs1) {
-            document.getElementById("cbutton1").style.display = "block";
-        } else if (cal1 == cs2) {
-            document.getElementById("cbutton2").style.display = "block";
-        } else if (cal1 == cs3) {
-            document.getElementById("cbutton3").style.display = "block";
-        } else if (cal1 == "") {
-            return;
-        } else {
-            numberleft[cal1]++;
-            numberleftdisplay();
+        if (cal1 != 0) {
+            if (cal1 == cs1) {
+                document.getElementById("cbutton1").style.display = "block";
+            } else if (cal1 == cs2) {
+                document.getElementById("cbutton2").style.display = "block";
+            } else if (cal1 == cs3) {
+                document.getElementById("cbutton3").style.display = "block";
+            } else if (cal1 == "") {
+                //return;
+            } else {
+                numberleft[cal1]++;
+                numberleftdisplay();
+            }
         }
-
         if (cal2 == "+") {
             counter = counter + mathcost[0] - 1;
             setCookie("counter", counter, "30");
@@ -88,18 +94,19 @@ function undo() {
         } else {
 
         }
-
-        if (cal3 == cs1) {
-            document.getElementById("cbutton1").style.display = "block";
-        } else if (cal3 == cs2) {
-            document.getElementById("cbutton2").style.display = "block";
-        } else if (cal3 == cs3) {
-            document.getElementById("cbutton3").style.display = "block";
-        } else if (cal3 == "") {
-            return;
-        } else {
-            numberleft[cal3]++;
-            numberleftdisplay();
+        if (cal3 != 0) {
+            if (cal3 == cs1) {
+                document.getElementById("cbutton1").style.display = "block";
+            } else if (cal3 == cs2) {
+                document.getElementById("cbutton2").style.display = "block";
+            } else if (cal3 == cs3) {
+                document.getElementById("cbutton3").style.display = "block";
+            } else if (cal3 == "") {
+                //return;
+            } else {
+                numberleft[cal3]++;
+                numberleftdisplay();
+            }
         }
     }
     cal1 = "";
